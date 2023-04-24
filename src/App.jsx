f1c863ee5ec7cabfc3d1343f81141e36efd0fb2d
NTN1789 <natanalmeida040@gmail.com>
2023-04-24 10:39:48 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react'
+import { api } from "./data"
+import './App.css'
+
+function App() {
+  const [pokemons, setPokemons] = useState([]);
+  const [next, setNext] = useState(null)
+  const [prev, setPrev] = useState(null)
+
+  useEffect(() => {
+    api
+      .get('pokemon')
+      .then((res) => {
+        setPokemons(res.data.results)
+        setNext(res.data.next)
+        setPrev(res.data.previous)
+      })
+      .catch((err) => console.log(err))
+
+
+  }, []);
+
+
+  const seeNext = (next) => {
+    api.get(next).then((res) => {
+      setPokemons(res.data.results)
+      setNext(res.data.next)
+      setPrev(res.data.previous)
+
+    })
+
+    window.scrollTo(0, 0)
+
+  }
+
+
+  const seePrev = (prev) => {
+    api.get(prev).then((res) => {
+      setPokemons(res.data.results);
+      setNext(res.data.next)
+      setPrev(res.data.previous)
+
+    })
+    window.scrollTo(0, 0)
+  }
+
+  const buildImgUrl = (url) => {
+    const id = url.split('/');
+    const idx = id.length - 2
+    const imgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id[idx]}.png`;
+
+    return imgUrl;
+
+  }
+
+
+  return (
+    <div className="container">
+      <div className='pokemon-container'>
+        {pokemons.map((pokemon) => {
+          return (
+            <div key={pokemon.name} className="pokemon">
+              <img src={buildImgUrl(pokemon.url)} alt={pokemon.name} />
+              <p> {pokemon.name}</p>
+            </div>
+          )
+        })}
+      </div>
+      <div className='button-container'>
+        <button
+          onClick={() => seePrev(prev)}
+          disabled={prev === null ? true : false}
+          className={prev === null ? 'btn-disabled ' : ""}
+        >
+          <i class='bx bx-left-arrow-alt'></i>
+        </button>
+
+        <button
+          onClick={() => seeNext(next)}
+          disabled={next === null ? true : false}
+          className={next === null ? "btn-disabled" : ""}
+        >
+
+          <i class='bx bx-right-arrow-alt' ></i>
+        </button>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
